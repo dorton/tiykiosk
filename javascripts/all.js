e2fdcdd2e4ac1c34522fe57334f4a46d50937257
Brian Dorton <dorton@gmail.com>
2016-01-29 21:43:07 -0600

    Automated commit at 2016-01-30 03:43:07 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -5894,6 +5894,7 @@ var Reveal = (function(){
     progress: false,
     history: true,
     center: true,
+    autoSlide: 10000,
     loop: true,
     autoSlideStoppable: false,
     theme: Reveal.getQueryHash().theme,
